6719210b1a053a460dcc7f9e109f763a27ed430b
gcaudle <gcaudle@c5-sven-host-01.caudle.local>
2020-09-22 02:11:09 -0500

    Committer: gcaudle <gcaudle@c5-sven-host-01.caudle.local> 	new file:   remoteku/__pycache__/main3.cpython-36.pyc 	modified:   remoteku/main2.py 	new file:   remoteku/main3.py Adding main3 in which working in drop downs for device selection

--- remoteku/main3.py
+++ remoteku/main3.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import *
 from tkinter import messagebox
 from tkinter.constants import *
 import api
@@ -20,7 +21,13 @@ lrTV = "http://192.168.0.200"
 sisTV = "http://192.168.1.199"
 parkTV = "http://192.168.1.198"
 response = {}
-devs = {}
+devs = {
+    "dadL": "http://192.168.0.111",
+    "dadR": "http://192.168.0.203",
+    "lrTV": "http://192.168.0.200",
+    "sisTV": "http://192.168.1.199",
+    "parkTV": "http://192.168.1.198"
+    }
     
 api_calls = {
     "device_info": "/query/device-info",
@@ -29,28 +36,6 @@ api_calls = {
     }
 
 alive_devs = []
-
-def api_req(dev, api_call):
-    import xmltodict
-    try:
-        r = requests.get(dev + api_port + api_call, timeout=10)
-    except Exception as e:
-        response = "Exception Thrown : {}".format(e)
-        print(e)
-        return response
-    except TimeoutError as timeout:
-        response = "Timeout Occured: {}".format(timeout)
-        print(timeout)
-        return response
-    else:
-        r_code = r.status_code
-        if r_code == 200:
-            print("REQUEST WAS A SUCCESS. DEVICE RETURNED: {} ".format(str(r)))
-            r2 = r.text
-            response = xmltodict.parse(r2, xml_attribs=False)
-            return response
-        else:
-            response = "ERROR"
 
 def dadspwr():
     for item in dadBOTH:
@@ -84,12 +69,25 @@ def pwrbtn_click(dev):
     msg_box(result)
 
 def pwr_status(dev):
-        api_call = "/query/device-info"
+        api_call = ":8060/query/device-info"
         try:
-            response = api_req(dev, api_call)
+            r = requests.get(dev, api_call, timeout=5)
         except TimeoutError as to_err:
-            response = "Timeout Error Occured on : {}".format(dev)
-            pass
+            print("Timeout Error Occured on : {}".format(dev))
+            pwr_status = "Unknown"
+            pwr_color = "red"
+            return pwr_color
+        except requests.exceptions.ConnectTimeout as tout_err:
+            print("ConnectTimeout Error Occured on : {}".format(dev))
+            pwr_status = "Unknown"
+            pwr_color = "red"
+            return pwr_color
+        except requests.exceptions.ConnectionError as conn_err:
+            print("Connect Error -No Route to Host- Occured on : {}".format(dev))
+            pwr_status = "Unknown"
+            pwr_color = "red"
+            return pwr_color
+        else:
             dev_info = response.get("device-info")
             pwr_state = dev_info.get("power-mode")
             if pwr_state == "Ready":
@@ -145,27 +143,41 @@ def msg_box(msg_label):
 # Triggering the start of the counter.
     count()
 
+values = ["None"]
+def list_devs():
+    index = 1
+    for key, value in devs.items():
+        entry = "#{} : {}".format(index, key)
+        values.insert(index, entry)
+        index = index + 1
+    return values
+values = list_devs()
+
 root = Tk()
 root.title("RemoteKu C5dev--..")
 root.minsize(width=250, height=70)
-msg_frame = LabelFrame(root, text = "Message Box")
-label = Label(msg_frame, text="Welcome")
-button1 = Button(root, text="L Pwr", bg="white", fg=pwr_status(dadL), command=lambda: pwrbtn_click(dadL))  # , padx=50, pady=100)
-button2 = Button(root, text="R Pwr", bg="white", fg=pwr_status(dadL), command=lambda: pwrbtn_click(dadR))
-button5 = Button(root, text="LR TV Pwr", bg="white", fg=pwr_status(lrTV), command=lambda: pwrbtn_click(lrTV))
-button3 = Button(root, text="Both Pwr", command=dadspwr)
-button6 = Button(root, text="Park Pwr", bg="white", fg="red", command=lambda: pwrbtn_click(parkTV))
-button7 = Button(root, text="Sis Pwr", bg="white", fg=pwr_status(sisTV), command=lambda: pwrbtn_click(sisTV))
-button4 = Button(root, text="Exit", command=root.destroy)
-button1.grid(row=1, column=0)
-button2.grid(row=1, column=1)
-button3.grid(row=1, column=2)
-button5.grid(row=2, column=0)
-button6.grid(row=2, column=1)
-button7.grid(row=2, column=2)
-button4.grid(row=4)
-msg_frame.grid(row=3, columnspan=10)#fill="both", expand="yes")
-#msg_frame_label.pack()
-label.pack()
+top_frame = Frame(root).pack()
+btm_frame = Frame(root).pack(side="bottom")
+combo1 = Combobox(top_frame)
+combo1["values"]=values
+combo1.current(0)
+combo1.pack()
+
+##button1 = Button(root, text="L Pwr", bg="white", command=lambda: pwrbtn_click(dadL))  # , padx=50, pady=100)
+##button2 = Button(root, text="R Pwr", bg="white", command=lambda: pwrbtn_click(dadR))
+##button5 = Button(root, text="LR TV Pwr", bg="white", command=lambda: pwrbtn_click(lrTV))
+##button3 = Button(root, text="Both Pwr", command=dadspwr)
+##button6 = Button(root, text="Park Pwr", bg="white", command=lambda: pwrbtn_click(parkTV))
+##button7 = Button(root, text="Sis Pwr", bg="white", command=lambda: pwrbtn_click(sisTV))
+##button4 = Button(root, text="Exit", command=root.destroy)
+##button1.grid(row=1, column=0)
+##button2.grid(row=1, column=1)
+##button3.grid(row=1, column=2)
+##button5.grid(row=2, column=0)
+##button6.grid(row=2, column=1)
+##button7.grid(row=2, column=2)
+##button4.grid(row=4)
+##msg_frame.grid(row=3, columnspan=10)#fill="both", expand="yes")
+##label.pack()
 
 root.mainloop()
