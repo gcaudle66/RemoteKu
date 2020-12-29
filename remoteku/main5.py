5d0af2bd7e069956c529e73723b4feb9a36a8d47
Garrett Caudle <gcaudle66@gmail.com>
2020-12-29 03:35:08 -0600

    now device list is based on reachability of dev. next multithread and status msgs

--- remoteku/main5.py
+++ remoteku/main5.py
@@ -9,6 +9,7 @@ import datetime
 import gui
 import concurrent.futures
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -56,7 +57,7 @@ dev_list2 = [["dadL", "http://192.168.0.111"],
     ["sisTV", "http://192.168.1.199"],
     ["parkTV", "http://192.168.1.198"]]
 
-
+input_list = ['InputTuner', 'InputHDMI1','InputHDMI2', 'InputHDMI3', 'InputHDMI4']
 
 dev_grps = {
     "dadBOTH": [dev_list.get("dadL"), dev_list.get("dadR")]
@@ -69,14 +70,41 @@ api_calls = {
     "active_app": "/query/active-app",
     "vol_up": "/keypress/volumeup",
     "vol_down": "/keypress/volumedown",
+    "vol_mute": "/keypress/volumemute",
+    "select": "/keypress/select",
+    "home": "/keypress/home",
+    "up": "/keypress/up",
+    "down": "/keypress/down",
+    "right": "/keypress/right",
+    "left": "/keypress/left",
+    "info": "/keypress/info",
     "input": "/keypress/inputhdmi{}".format(cur_hdmi)
     }
 
-def vals(dev_list):
+def inputs(input_list):
+    inp_vals = []
+    for value in input_list.values():
+        inp_vals.append(value)
+    return inp_vals
+
+
+def dev_check(dev_list):
+    dev_states = []
+    dev_states = dev_status()
+    return vals(dev_states)
+
+def vals(dev_states):
     val_list = []
-    for value in dev_list.values():
-        val_list.append(value)
+    for value in dev_states:
+        if value[2] != 'red':
+            val_list.append(value[0])
     return val_list
+
+##def vals(dev_states):
+##    val_list = []
+##    for value in dev_list.values():
+##        val_list.append(value)
+##    return val_list
 
 def keypress(dev, key):
     r = api_req(dev, "POST", key)
@@ -92,7 +120,7 @@ def keypress(dev, key):
 @logger_func
 def api_post(dev, api_call):
     """
-    Function for api GET calls
+    Function for api POST calls
     """
     import xmltodict
     import pdb
@@ -111,8 +139,8 @@ def api_post(dev, api_call):
     if r_code == 200:
         print("REQUEST WAS A SUCCESS. DEVICE RETURNED: {} ".format(str(r)))
         r2 = r.text
-        response = r_code
-        return response
+        response = f'{r_code} - OK'
+        return msg_box(response)
 
 @logger_func    
 def api_req(dev, api_call):
@@ -121,9 +149,8 @@ def api_req(dev, api_call):
     """
     import xmltodict
     import logging
-    import pdb
     try:
-        r = request.get(dev + ':8060' + api_call, timeout=10)
+        r = requests.get(dev + ':8060' + api_call, timeout=3)
     except Exception as exc:
         response = ["ERR", exc]
         return response[0]
@@ -180,12 +207,14 @@ def active_app(dev):
     act_app = response.get("active-app").get("app")
     return act_app
 
-def dev_status(dev):
+def dev_status():
+    dev_states = []
     for key,value in dev_list.items():
         dev_url = value
-        result = threader(value, pwr_status)
+        result = pwr_status(value)
         dev_status = (result)
-        return dev_status
+        dev_states.append(dev_status)
+    return dev_states
 
 def pwr_status(dev):
         api_call = "/query/device-info"
@@ -195,21 +224,26 @@ def pwr_status(dev):
             response = "Timeout Error Occured on : {}".format(dev)
             pwr_status = "Unknown"
             pwr_color = "red"
-            return pwr_status, pwr_color
+            return dev, pwr_status, pwr_color
+        if response == 'ERR':
+            response = "Timeout2 Error Occured on : {}".format(dev)
+            pwr_status = "Unknown"
+            pwr_color = "red"
+            return dev, pwr_status, pwr_color
         dev_info = response.get("device-info")
         pwr_state = dev_info.get("power-mode")
         if pwr_state == "Ready":
             pwr_status = "Sleep"
             pwr_color = "orange"
-            return pwr_status, pwr_color
+            return dev, pwr_status, pwr_color
         elif pwr_state == "PowerOn":
             pwr_status = "On"
             pwr_color = "green"
-            return pwr_status, pwr_color
+            return dev, pwr_status, pwr_color
         else:
             pwr_status = "Unknown"
             pwr_color = "red"
-            return pwr_status, pwr_color
+            return dev, pwr_status, pwr_color
 
 
 
@@ -222,111 +256,96 @@ def input_hdmi_cycle(dev, cur_hdmi):
     response = api_post(dev, api_calls.get("input"), cur_hdmi)
     return response
 
-##    if cur_hdmi < 4:
-##        cur_hdmi = cur_hdmi + 1
-##        r = api_post(dev, api_calls.get("input" + str(cur_hdmi)))
-##        if cur_hdmi in hdmi_range:
-##            print("Value for cur_hdmi:{} in range".format(cur_hdmi))
-##            return cur_hdmi
-##        else:
-##            print("ERROR: INPUT value out of range")
-##            cur_hdmi = 1
-##            return cur_hdmi
-##    else:
-##            cur_hdmi = 1
-##            return cur_hdmi
-
-
 def select_dev(eventObject):
     device = eventObject.get()
     label1["text"] = "OK"
-    return device 
+    return device
+
+def toplevel_input():
+    ii = tkinter.StringVar()
+    toplevel1 = tkinter.Toplevel(root)
+    toplevel1.title('RemoteKu-Input Selector')
+    input_combobox = ttk.Combobox(toplevel1, textvariable=ii)
+    input_combobox['values'] = inputs(input_list)
+    input_combobox.grid()
+    toplevel1.bind('<<ComboboxSelected>>', select_input)
+    return toplevel1
+
+def select_input(eventObject):
+##    ii = eventObject.get()
+    toplevel1.destroy()
+    return ii
     
 ############## Below is GUI definitions
 root = Tk()
 root.title("RemoteKu C5dev--..")
 root.minsize(width=100, height=70)
 
+style1 = ttk.Style()
+style1.map("C.TButton",
+    foreground=[('pressed', 'red'), ('active', 'blue')],
+    background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+    )
+style1.map("C.TCombobox",
+    foreground=[('pressed', 'red'), ('active', 'red'), ('focus', 'red')],
+    focusfill=[('pressed', 'red'), ('active', 'red'), ('focus', 'red')],
+    fieldbackground=[('pressed', 'red'), ('active', 'red'), ('focus', 'red')],
+    background=[('pressed', '!disabled', 'red'), ('active', 'red')])
 top = ttk.Frame(root)
 top.grid(columnspan=2, rowspan=2)
 
 label1 = ttk.Label(top, text='Current Device').grid(column=0, row=1, pady=2)
 
 n = tkinter.StringVar()
-current_dev = ttk.Combobox(top, textvariable=n)
-current_dev['values'] = vals(dev_list)
-current_dev.current(2)
+current_dev = ttk.Combobox(top, textvariable=n, style="C.TCombobox")
+current_dev['values'] = dev_check(dev_list)
+##current_dev.current(2)
 current_dev.grid()
 top.bind('<<ComboboxSelected>>', select_dev)
 
 device = n.get()
+
+##ii = tkinter.StringVar()
+##toplevel1 = tkinter.Toplevel(root)
+##toplevel1.title('RemoteKu-Input Selector')
+##input_combobox = ttk.Combobox(toplevel1, textvariable=ii)
+##input_combobox['values'] = inputs(input_list)
+##input_combobox.grid()
 
 sep1 = ttk.Separator(root, orient='horizontal').grid(row=2)
 
 index = ttk.Frame(root).grid(columnspan=3)
 ##########Current Tab Config Btns etc
 
-btn1 = ttk.Button(index, text="Pwr", command=lambda:api_post(n.get(),api_calls.get("power_cycle"))).grid(row=3, column=0)
-btn2 = ttk.Button(index, text=" ^ ").grid(row=3, column=1)
-btn3 = ttk.Button(index, text="Input", command=lambda:input_hdmi_cycle(n,cur_hdmi)).grid(row=3, column=2)
+btn1 = ttk.Button(index, style='C.TButton', text="Pwr", command=lambda:api_post(n[1].get(),api_calls.get("power_cycle"))).grid(row=3, column=0)
+btn2 = ttk.Button(index, text=" ^ ", command=lambda:api_post(n.get(),api_calls.get("up"))).grid(row=3, column=1)
+btn3 = ttk.Button(index, text="Input", command=toplevel_input).grid(row=3, column=2)#lambda:input_hdmi_cycle(n,cur_hdmi)).grid(row=3, column=2)
 
-btn4 = ttk.Button(index, text=" < ").grid(row=4, column=0)
-btn5 = ttk.Button(index, text="Enter").grid(row=4, column=1)
-btn6 = ttk.Button(index, text=" > ").grid(row=4, column=2)
+btn4 = ttk.Button(index, text=" < ", command=lambda:api_post(n.get(),api_calls.get("left"))).grid(row=4, column=0)
+btn5 = ttk.Button(index, text="Enter", command=lambda:api_post(n.get(),api_calls.get("select"))).grid(row=4, column=1)
+btn6 = ttk.Button(index, text=" > ", command=lambda:api_post(n.get(),api_calls.get("right"))).grid(row=4, column=2)
 
-btn7 = ttk.Button(index, text=" ").grid(row=5, column=0)
-btn8 = ttk.Button(index, text="\/").grid(row=5, column=1)
+btn7 = ttk.Button(index, text="Mute", command=lambda:api_post(n.get(),api_calls.get("vol_mute"))).grid(row=5, column=0)
+btn8 = ttk.Button(index, text="\/", command=lambda:api_post(n.get(),api_calls.get("down"))).grid(row=5, column=1)
 btn9 = ttk.Button(index, text="Vol Up", command=lambda:api_post(n.get(),api_calls.get("vol_up"))).grid(row=5, column=2)
 
-btn10 = ttk.Button(index, text="Pwr L+R ", command=dadspwr).grid(row=6, column=0)
-btn11= ttk.Button(index, text="Stat").grid(row=6, column=1)
+btn10 = ttk.Button(index, text="Home", command=lambda:api_post(n.get(),api_calls.get("home"))).grid(row=6, column=0)
+btn11= ttk.Button(index, text="Info", command=lambda:api_post(n.get(),api_calls.get("info"))).grid(row=6, column=1)
 btn12 = ttk.Button(index, text="Vol Dn", command=lambda:api_post(n.get(),api_calls.get("vol_down"))).grid(row=6, column=2)
 
 
 msg_frame1 = LabelFrame(root, text = "Message Box")
 
-
-
+msg_initial = "Welcome"
+label1 = Label(msg_frame1)
+label1['text'] = msg_initial
+label1.grid()
+msg_frame1.grid(sticky="s", columnspan=3)
 
 def msg_box(msg_label):
-            def count():
-                if running:
-                    global counter
+    counter = 3
+    label1['text'] = msg_label
+    return label1
 
-        # To manage the intial delay.
-                    if counter==-1:
-                        display=msg_label
-                    elif counter > 20:
-                        display=""
-                    else:
-                        display=msg_label#str(counter)
-                        timing = display
 
-                    label1['text']=display # Or label.config(text=display)
-
-        # label.after(arg1, arg2) delays by
-        # first argument given in milliseconds
-        # and then calls the function given as second argument.
-        # Generally like here we need to call the
-        # function in which it is present repeatedly.
-        # Delays by 1000ms=1 seconds and call count again.
-                    label1.after(1000, count)
-                    counter += 10
-                else:
-                    if counter >= 1:
-                        display = (counter)
-                        timing = display
-                        print("DEBUG: The timing var shows " + str(timing))
-                        print("DEBUG: Timer clocked  " + str(counter))
-                        label1['text']=display
-                        
-                        print("DEBUG: sending " + str(display) + " to convert_time....")
-                        return convert_time(counter)
-                    else:
-                        print("Value to low")
-        # Triggering the start of the counter.
-            count()
-    
-label1 = Label(msg_frame1, text="Welcome").pack()
-msg_frame1.grid(sticky="s", columnspan=3)
 root.mainloop()
